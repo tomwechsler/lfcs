5f49e0fada517cd5413c389159018bc39de2dbcf
Tom Wechsler <tom.wechsler@outlook.com>
2023-11-04 13:28:33 +0100

    new files

---
+++ Ansible_installation.sh
@@ -0,0 +1,10 @@
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+
+sudo apt install ansible 
+
+file $(which ansible) 
+
+ansible --version
+
+Although Ansible is part of the standard repositories, it is an early version, to install a later
+version of Ansible we can add in the Ansible repository to Ubuntu 20.04.
