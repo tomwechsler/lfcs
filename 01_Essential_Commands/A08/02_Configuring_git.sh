74107dfcc52c05ac82e110721b00e7a4d8c09c76
Tom Wechsler <tom.wechsler@outlook.com>
2023-11-05 16:12:59 +0100

    new file

---
+++ 01_Essential_Commands/A08/02_Configuring_git.sh
@@ -0,0 +1,20 @@
+#Working on ubuntu1
+
+#List the git config
+git config --list
+
+#The global git config
+git config --global --list
+
+#List all files
+ls -a
+
+#Configure the git global settings
+git config --global user.name "Tom Wechsler"
+git config --global user.email "tom@example.com"
+
+#The global git config
+git config --global --list
+
+#The repo git config
+git config --list
