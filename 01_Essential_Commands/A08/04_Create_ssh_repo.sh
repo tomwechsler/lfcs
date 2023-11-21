fe23ab8460a351974978ce3fc41d5e42f1c9c468
Tom Wechsler <tom.wechsler@outlook.com>
2023-11-21 06:59:39 +0100

    new files

--- 01_Essential_Commands/A08/04_Create_ssh_repo.sh
+++ 01_Essential_Commands/A08/04_Create_ssh_repo.sh
@@ -9,4 +9,41 @@ echo 'git:[P@ssw0rd]' | sudo chpasswd
 #Create a SSH key pair
 ssh-keygen -N "" -f /home/vagrant/.ssh/id_rsa
 
+#Copy the public key to the git user
+ssh-copy-id git@localhost
 
+#SSH to the git user
+ssh git@localhost
+
+#Create a git repository
+mkdir -p git/project1.git
+
+#Change the directory to the git repository
+cd git/project1.git
+
+#List the files
+ls -la
+
+#Initialize the git repository (--bare just create the database)
+git init --bare
+
+#Leave the SSH session
+exit
+
+#Clone the git repository
+git clone git@localhost:/home/git/git/project1
+
+#Change the directory to the git repository
+cd project1
+
+#Create a file
+echo "Hello World" > hello.sh
+
+#Add the file to the git repository
+git add .
+
+#Commit the file
+git commit -m "First script version"
+
+#Push the file to the git repository
+git push
---
+++ 01_Essential_Commands/A08/05_Using_versioning_tags.sh
@@ -0,0 +1,61 @@
+#Working on ubuntu1
+
+#Change the directory
+cd project1
+
+#The status
+git status
+
+#Add a tag
+git tag v1.0
+
+#The status
+git status
+
+#Git show
+git show
+
+#Git push
+git push origin v1.0
+
+#Show the tags
+git tag
+
+#Git show
+git show v1.0
+
+#Change to home directory
+cd ~
+
+#Remove the project1 directory
+rm -rf project1
+
+#Clone the git repository with the tag
+git clone -b v1.0 git@localhost:/home/git/git/project1
+
+#Change the directory
+cd project1
+
+#Show the files
+ls -la
+
+#Show the content of the file
+cat hello.sh
+
+#The git status
+git status
+
+#Change to home directory
+cd ~
+
+#Remove the project1 directory
+rm -rf project1
+
+#Clone the git repository with the tag
+git clone git@localhost:/home/git/git/project1
+
+#Change the directory
+cd project1
+
+#The git status
+git status
