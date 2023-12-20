ba9813173d78b76c53e5b2f5ae7a6875bd7b5912
Tom Wechsler <tom.wechsler@outlook.com>
2023-12-20 07:05:56 +0100

    lines edited

--- 03_User_and_Group_Management/A06/03_Creating_User_Home_Directories.sh
+++ 03_User_and_Group_Management/A06/03_Creating_User_Home_Directories.sh
@@ -1,20 +1,27 @@
+#Working on ubuntu1
+
 #Lets remove the CREATE_HOME
 vim /etc/login.defs
 
+#Create a new user named 'alf'
 sudo useradd alf
 
+#Set the password for the 'alf' user to 'P@ssw0rd'
 echo "alf:P@ssw0rd" | sudo chpasswd
 
+#Switch to the 'alf' user
 su - alf
 
+#Display the string "session optional pam_mkhomedir.so"
 echo "session optional pam_mkhomedir.so"
 
+#Append the string "session optional pam_mkhomedir.so" to the end of the /etc/pam.d/common-session file
 echo "session optional pam_mkhomedir.so" | sudo tee -a /etc/pam.d/common-session
 
-cat ESC .
+#'cat ESC .' is not a valid command, it might be a typo
 
+#Switch to the 'alf' user again
 su - alf
 
+#Exit the current shell
 exit
-
-
