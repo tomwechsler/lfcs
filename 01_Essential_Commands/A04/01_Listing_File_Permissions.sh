1948452786a26107f680a63064408fcabd2c883b
Tom Wechsler <tom.wechsler@outlook.com>
2024-06-10 14:40:07 +0200

    lines edited

--- 01_Essential_Commands/A04/01_Listing_File_Permissions.sh
+++ 01_Essential_Commands/A04/01_Listing_File_Permissions.sh
@@ -6,18 +6,23 @@ ls -l /etc/shadow
 
 <file type> <permissions> <link count> <user group> <size> <modified time>
 
+#List the '/etc/services' file in long format, which includes detailed information such as file permissions, number of links, owner, group, size, and time of last modification
 ls -l /etc/services
 
+#List the '/etc/services' file in long format with file sizes in human readable format (e.g., KB, MB)
 ls -lh /etc/services
 
+#Open the '/etc/services' file with vim for editing (requires sudo permissions)
 sudo vim /etc/services
 
+#List the '/etc/services' file in long format with file sizes in human readable format again, to see if any changes were made
 ls -lh /etc/services
 
+#Display detailed status of the '/etc/services' file
 stat /etc/services 
 
+#Display the file permissions of the '/etc/services' file in octal format
 stat -c %a /etc/services
-644
 
+#Display the file permissions of the '/etc/services' file in human readable format
 stat -c %A /etc/services
-
